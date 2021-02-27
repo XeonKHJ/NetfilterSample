e2523b2f87c4d0e2c62b09c2691a445241f58f4c
康宏嘉 <redalertkhj@live.cn>
2021-02-28 00:14:10 +0800

    更新忽略列表。

--- netfiltersample.c
+++ netfiltersample.c
@@ -1,84 +1,28 @@
 #include <linux/module.h>  
 #include <linux/kernel.h>  
-#include <linux/init.h>  
 #include <linux/types.h>  
 #include <linux/netdevice.h>  
-#include <linux/skbuff.h>  
 #include <linux/netfilter_ipv4.h>  
-#include <linux/inet.h>  
 #include <linux/in.h>  
 #include <linux/ip.h>  
 #include <linux/tcp.h>  
 #include <linux/udp.h>  
 
 MODULE_LICENSE("GPL"); 
-MODULE_AUTHOR("XuShuzhan");
+MODULE_AUTHOR("Kang Hongjia");
 MODULE_DESCRIPTION("A NetFilter Demo");
 MODULE_VERSION("1.0"); 
 
-#define NIPQUAD(addr)  \
-  ((unsigned char *)&addr)[0], \
-  ((unsigned char *)&addr)[1],  \
-  ((unsigned char *)&addr)[2],  \
-  ((unsigned char *)&addr)[3]  
-
-struct net net1;
-struct net net2;
-
-static unsigned int filter_GET_POST(void *p, struct sk_buff *skb, const struct nf_hook_state *s)
-{  
- __be32 sip,dip;  
- if(skb){  
-   char *data = NULL; //tcp data
-
-   char *request_method;//http request method id request line
-
-   struct sk_buff *sb = NULL;  
-   struct tcphdr *tcph = NULL; 
-   sb = skb;  
-   struct iphdr *iph;  
-   iph  = ip_hdr(sb);
-   tcph = tcp_hdr(sb);     
-   sip = iph->saddr;  //source ip address
-   dip = iph->daddr;  //destination ip address
-
-   //(64_bit pc) get tcp's data pointor 
-   data = (unsigned long)tcph + (unsigned long)(tcph -> doff * 4);
-
-   if(request_method = strstr(data,"GET")!=NULL){
-  printk(">>GET>>>>GET>>>>>GET>>>>>>>>>GET>>>>>>>>>>>>>GET>>>>>>>>>>>> \n");
-        printk("[GET]  Packet from: %d.%d.%d.%d to %d.%d.%d.%d ", NIPQUAD(sip), NIPQUAD(dip));
-  printk("GET -> %s \n",data);
-
-   }else if(request_method = strstr(data,"POST")!=NULL){
-  printk(">>POST>>>>POST>>>>>POST>>>>>>>>>POST>>>>>>>>>>>>>POST>>>>>>>>>>>> \n");
-  printk("[POST] Packet from: %d.%d.%d.%d to %d.%d.%d.%d", NIPQUAD(sip), NIPQUAD(dip));
-  printk("POST -> %s \n",data);
-
-   }
-  }  
-   return NF_ACCEPT;  
+static unsigned int HookFunc(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+{
+    __be32 srcIp, destIp;
+    if(skb)
+    {
+        
+    }
 }
 
- //regist a hook function 
- struct nf_hook_ops filter_GET_POST_ops = {  
-   .hook = filter_GET_POST,  //hooked function
-   .pf = NFPROTO_IPV4,  
-   .hooknum = NF_INET_LOCAL_OUT, 
-   .priority = NF_IP_PRI_FILTER+2  
- };  
-
-static int __init filter_GET_POST_init(void) {  
-  printk("ShadowDriver kernel module loaded.\n");
-  nf_register_net_hook(&net1, &filter_GET_POST_ops);  
-  return 0;  
-}  
-
-
-static void __exit filter_GET_POST_exit(void) {  
-  printk("ShadowDriver kernel module unloaded.\n");
-  nf_unregister_net_hook(&net2, &filter_GET_POST_ops);  
-}   
+nf_hook_ops
 
 module_init(filter_GET_POST_init);  
 module_exit(filter_GET_POST_exit);   
